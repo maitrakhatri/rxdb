609d281ee9ad3f4647a754d48a6c6145d8ca1213
maitrakhatri <maitrakhatri@gmail.com>
2023-08-08 14:05:33 +0530

    feat: send a doc to couchDB after 10 updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,50 @@
 import { useState } from "react";
 import "./App.css";
 import Tiptap from "./TipTap";
-import { fileversedb } from "./database";
-import { Content } from "@tiptap/react";
+// import { fileversedb } from "./database";
+// import { Content } from "@tiptap/react";
+import axios from "axios";
 
 function App() {
   const [id, setId] = useState<number>(0);
-  const [loadedContent, setLoadedContent] = useState<Content>();
+  // const [loadedContent, setLoadedContent] = useState<Content>();
+  const [latestRev, setLatestRev] = useState<string>();
 
-  const getData = async (id: number) => {
-    const foundDocuments = await fileversedb.collabdocs
-      .find({
-        selector: {
-          id: {
-            $eq: id,
-          },
-        },
-        sort: [{ timestamp: "desc" }],
-      })
-      .exec();
+  // const getData = async (id: number) => {
+  //   const foundDocuments = await fileversedb.collabdocs
+  //     .find({
+  //       selector: {
+  //         id: {
+  //           $eq: id,
+  //         },
+  //       },
+  //       sort: [{ timestamp: "desc" }],
+  //     })
+  //     .exec();
 
-    setLoadedContent(foundDocuments[0]._data.content);
+  //   setLoadedContent(foundDocuments[0]._data.content);
+  // };
+
+  const createDatabase = async () => {
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:5984/fileversedb/${id}`,
+        {
+          content: {},
+        }
+      );
+      setLatestRev(response.data.rev);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
   return (
     <div className="App">
       <input type="number" onChange={(e) => setId(Number(e.target.value))} />
-      <button onClick={() => getData(id)}>Get Data</button>
-      <Tiptap id={id} loadedContent={loadedContent} />
+      {/* <button onClick={() => getData(id)}>Get Data</button> */}
+      <button onClick={() => createDatabase()}>Create database</button>
+      {latestRev && <Tiptap id={id} rev={latestRev} />}
     </div>
   );
 }
--- src/TipTap.tsx
+++ src/TipTap.tsx
@@ -1,29 +1,66 @@
 import { useEditor, EditorContent, Content } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { fileversedb } from "./database";
-import { useEffect } from "react";
-
-const addData = async ({ content, id }: { content: unknown; id: number }) => {
-  await fileversedb.collabdocs.insert({
-    id,
-    content,
-    timestamp: new Date().toISOString(),
-  });
-};
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Tiptap = ({
   id,
   loadedContent,
+  rev,
 }: {
   id: number;
-  loadedContent: Content | undefined;
+  loadedContent?: Content | undefined;
+  rev: string;
 }) => {
+  const [counter, setCounter] = useState<number>(0);
+  const [latestRev, setLatestRev] = useState<string>(rev);
+  const [editorContent, setEditorContent] = useState<Content>();
+
+  const sendToCouchdb = async ({
+    latestRev,
+    content,
+  }: {
+    latestRev: string;
+    content: Content;
+  }) => {
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:5984/fileversedb/${id}`,
+        {
+          _rev: latestRev,
+          content,
+        }
+      );
+      setLatestRev(response.data.rev);
+      setCounter(0);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (counter === 10) {
+      if (editorContent) sendToCouchdb({ latestRev, content: editorContent });
+    }
+  }, [counter]);
+
+  const addData = async ({ content, id }: { content: unknown; id: number }) => {
+    await fileversedb.collabdocs.insert({
+      id,
+      content,
+      timestamp: new Date().toISOString(),
+    });
+    setCounter((counter) => counter + 1);
+  };
+
   const editor = useEditor({
     extensions: [StarterKit],
     content: "<p> New Document </p>",
     onUpdate: () => {
       const content = editor?.getJSON();
       addData({ content, id });
+      setEditorContent(content);
     },
   });
 
